fad09ac68703187c9c51a90ddbd7423ae507b268
Marko Ivković <marko.ivkovic92@gmail.com>
2022-07-20 19:03:13 +0200

    console JS game of Rock Paper Scissors

---
+++ script.js
@@ -0,0 +1,61 @@
+function game(){
+  const games = 5;  
+  let playerPoints = 0; 
+  let computerPoints = 0; 
+
+  for(let i = 0; i < games; i++) {
+    let playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
+    function computerPlay() { return (["paper","scissors","rock"])[Math.random() * 3 | 0]; }
+    let computerSelection = computerPlay();
+
+    let round = playRound(playerSelection, computerSelection);
+    if(round === "wins"){
+      playerPoints++;
+      console.log("You Win");
+    } else if(round === "loses"){
+      computerPoints++;
+      console.log("You Lose");
+    } else if(round === "ties") {
+      console.log("tied");
+    } 
+  }
+
+  if(playerPoints > computerPoints){
+    console.log('');
+    console.log("Congratulations!  You beat the computer " + playerPoints + " to " + computerPoints);
+  } else {
+    console.log("Sorry!  You lost to the computer " + computerPoints + " to " + playerPoints);
+  }
+} 
+
+function playRound(playerSelection, computerSelection) {  
+  if (playerSelection == 'rock') {
+    if (computerSelection == 'scissors') {
+      return 'wins';
+    } else if (computerSelection == 'paper') {
+      return 'loses';
+    } else {
+      return 'ties';
+    } 
+  }
+  if (playerSelection == 'paper') {
+    if (computerSelection == 'rock') {
+      return 'wins';
+    } else if (computerSelection == 'scissors') {
+      return 'loses';
+    } else {
+      return 'ties';
+    } 
+  }
+  if (playerSelection == 'scissors') {
+    if (computerSelection == 'rock') {
+      return 'loses';
+    } else if (computerSelection == 'paper') {
+      return 'wins';
+    } else {
+      return 'ties';
+    } 
+  }
+}
+
+game();
