f4dcbce5514adc58edfc64fe04c1967a1c1a78aa
Marko Ivković <marko.ivkovic92@gmail.com>
2022-07-20 22:34:37 +0200

    second commit

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
+const choices = ["rock", "paper", "scissors"]
+
 function game(){
   const games = 5;  
   let playerPoints = 0; 
   let computerPoints = 0; 
 
   for(let i = 0; i < games; i++) {
-    let playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
+    let playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase(); 
     function computerPlay() { return (["paper","scissors","rock"])[Math.random() * 3 | 0]; }
     let computerSelection = computerPlay();
 
@@ -29,32 +31,40 @@ function game(){
 } 
 
 function playRound(playerSelection, computerSelection) {  
-  if (playerSelection == 'rock') {
-    if (computerSelection == 'scissors') {
+  if (playerSelection === 'rock' && playerSelection === choices[0]) {
+    if (computerSelection ==='scissors') {
       return 'wins';
-    } else if (computerSelection == 'paper') {
+    } else if (computerSelection === 'paper') {
       return 'loses';
     } else {
       return 'ties';
     } 
+  } else {
+    prompt ('Wrong choice! Type again')
   }
-  if (playerSelection == 'paper') {
-    if (computerSelection == 'rock') {
+
+  if (playerSelection === 'paper' && playerSelection === choices[1]) {
+    if (computerSelection === 'rock') {
       return 'wins';
-    } else if (computerSelection == 'scissors') {
+    } else if (computerSelection === 'scissors') {
       return 'loses';
     } else {
       return 'ties';
     } 
+  } else {
+    prompt ('Wrong choice! Type again')
   }
-  if (playerSelection == 'scissors') {
-    if (computerSelection == 'rock') {
+
+  if (playerSelection === 'scissors' && playerSelection === choices[2]) {
+    if (computerSelection === 'rock') {
       return 'loses';
-    } else if (computerSelection == 'paper') {
+    } else if (computerSelection === 'paper') {
       return 'wins';
     } else {
       return 'ties';
     } 
+  } else {
+    prompt ('Wrong choice! Type again')
   }
 }
 
